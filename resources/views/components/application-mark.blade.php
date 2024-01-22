e8f641aface95104233bef8c5a073f989e609c8c
jespersch <jesperschilte@hotmail.com>
2024-01-22 10:24:42 +0100

    Created a working Loginsystem using Jetstream. Also altered all the logo's to the Naikiedas logo

---
+++ resources/views/components/application-mark.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <img style="max-width:85px;" src="{{ asset('img/logo.png') }}" alt="Logo">
+
+</div>
---
+++ resources/views/components/authentication-card-logo.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <img src="{{ asset('img/logo.png') }}" alt="Logo">
+
+</div>
