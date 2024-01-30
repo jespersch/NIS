b99862244ede2e1bd0f47b590c85568866fc9a35
Ricardo Jutte <rico@MacBook-Pro-van-Ricardo.local>
2024-01-30 09:44:17 +0100

    dashboard update

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BegroetingController;
+use App\Http\Controllers\DashboardController;
+
+
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
-|--------------------------------------------------------------------------
+|----------------------------------------------------------------------x  ----
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
@@ -17,14 +21,14 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Route::get('/', function () { return view('dashboard'); })->middleware(['auth'])->name('dashboard');
+Route::get('/', function () {
+  return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
 Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
+  'auth:sanctum',
+  config('jetstream.auth_session'),
+  'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+  Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 });
